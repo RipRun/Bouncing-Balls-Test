7e8e2d72c26d4a01fec430f44d3cda4ce800dd18
RipRun <ripple.erin@comcast.net>
2018-08-12 23:30:31 -0700

    Test

--- Bouncing-Balls-Test.py
+++ Bouncing-Balls-Test.py
@@ -1,4 +1,4 @@
-import pygame
+import sys, pygame, random
 from pygame.locals import *
 
 pygame.init()
@@ -13,12 +13,31 @@ color = (26, 255, 255)
 ball_image = pygame.image.load("ball-6x6.png")
 ball_image = pygame.transform.smoothscale(ball_image, (30, 30))
 ball_rect = ball_image.get_rect()
+ball_rect.center = (width//2, height//2)
+speed = pygame.math.Vector2(0, 20)
+speed.rotate_ip(random.randint(0, 360))
+
+
+def move_ball():
+    # get information from screen in case of resizing
+    screen_info = pygame.display.Info()
+    ball_rect.move_ip(speed)
+    if ball_rect.left < 0 or ball_rect.right > screen_info.current_w:
+        speed[0] *= -1
+        ball_rect.move_ip(speed[0], 0)
+    if ball_rect.top < 0 or ball_rect.bottom > screen_info.current_h:
+        speed[1] *= -1
+        ball_rect.move_ip(0, speed[1])
 
 
 def main():
     while True:
         # program will never run at more than 60 frames/sec
         clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+        move_ball()
         screen.fill(color)
         screen.blit(ball_image, ball_rect)
         # update the full display to the screen
